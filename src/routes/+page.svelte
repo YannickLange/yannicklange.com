6fc6b1b2629608fe6d404c0a99ca9096f1f5a6f0
Yannick Lange <yannick.lange.94@gmail.com>
2023-12-14 20:01:45 +0100

    layout with header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,8 @@
 <script>
     import '$lib/styles/style.css';
-    import Header from '$lib/components/Header.svelte';
 </script>
 
 <body>
-    <Header/>
     <h2>Yannick Lange</h2>
     <p>hello</p>
 </body>

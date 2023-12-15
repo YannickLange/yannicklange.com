afc196a75131c8436625cbe8ac8daf3ccf867788
Yannick Lange <yannick.lange.94@gmail.com>
2023-12-15 12:47:52 +0100

    blog list of all posts

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let data;
+</script>
+
+<ul>
+    {#each data.posts as post}
+        <li>
+            <h2>
+                <a href={post.path}>
+                    {post.meta.title}
+                </a>
+            </h2>
+            published {post.meta.date}
+        </li>
+    {/each}
+</ul>

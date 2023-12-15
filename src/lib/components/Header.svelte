118303a2f536a313d16788154b453e52fd2281a0
Yannick Lange <yannick.lange.94@gmail.com>
2023-12-15 09:23:46 +0100

    header as sidebar

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,24 @@
+<style>
+    header {
+        box-sizing: border-box;
+        padding: 20px;
+    }
+    ul {
+        list-style-type: none;
+        gap: 1rem;
+    }
+</style>
+
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="/">home</a>
+            </li>
+            <li>
+                <a href="/resume">resume</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Header from '$lib/components/Header.svelte';
+</script>
+
+<style>
+  /* You can add more styles specific to your component here */
+
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: stretch;
+    height: 100vh;
+  }
+
+  .content {
+    flex-grow: 1;
+    max-width: 800px; /* Adjust the maximum width of the content area */
+    padding: 20px;
+    box-sizing: border-box;
+  }
+</style>
+
+<main>
+    <div class="container">
+        <Header/>
+        <div class="content">
+            <slot/>
+        </div>
+    </div>
+</main>
+

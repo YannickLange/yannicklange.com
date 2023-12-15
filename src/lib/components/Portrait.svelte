ce7fd10905b84713eb5e8b50c259206f072dc192
Yannick Lange <yannick.lange.94@gmail.com>
2023-12-15 13:44:12 +0100

    portrait

---
+++ src/lib/components/Portrait.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<style>
+    .circle-image {
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        overflow: hidden;
+    }
+
+    .circle-image img {
+        width: 100%;
+        height: auto;
+        display: block;
+    }
+</style>
+
+<div class="circle-image">
+    <img src="me.jpeg" alt="portrait" width="500" height="500">
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
     import '$lib/styles/style.css';
+    import Portrait from '$lib/components/Portrait.svelte';
 </script>
 
 <body>
+    <Portrait/>
     <h2>Yannick Lange</h2>
     <p>hello</p>
 </body>

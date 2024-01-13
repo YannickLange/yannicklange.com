b75e4396baecc8230815451988a361817f8cb927
Yannick Lange <yannick.lange.94@gmail.com>
2024-01-13 13:35:14 +0100

    render mermaid diagram

---
+++ src/lib/components/Code.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { onMount } from 'svelte';
+    import mermaid from 'mermaid';
+
+    export let code = '';
+
+    onMount(() => {
+        mermaid.initialize({ startOnLoad: true });
+        mermaid.contentLoaded();
+    });
+</script>
+
+<div class="mermaid">
+    {code}
+</div>
+
+<style>
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <script>
     import '$lib/styles/style.css';
     import Portrait from '$lib/components/Portrait.svelte';
+    import Code from '$lib/components/Code.svelte';
+    let mermaidCode = 'flowchart TB; A & B --> C & D';
 </script>
 
 <Portrait/>
 <h2>Yannick Lange</h2>
 <p>hello</p>
+<Code code={mermaidCode}/>
+
+

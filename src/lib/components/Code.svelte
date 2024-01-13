1f50112548aeca814c9994ce9545b64ab35f346b
Yannick Lange <yannick.lange.94@gmail.com>
2024-01-13 14:04:24 +0100

    slot for mermaid code

--- src/lib/components/Code.svelte
+++ src/lib/components/Code.svelte
@@ -1,17 +1,30 @@
 <script>
     import { onMount } from 'svelte';
+    import { writable } from 'svelte/store';
     import mermaid from 'mermaid';
 
-    export let code = '';
+    let container;
+    let rendered = writable(false);
 
-    onMount(() => {
-        mermaid.initialize({ startOnLoad: true });
-        mermaid.contentLoaded();
+    onMount(async () => {
+        mermaid.initialize({ theme: 'neutral', startOnLoad: false });
+        rendered.set(true); 
+        setTimeout(async () => {
+            await mermaid.run()
+        }, 0)
     });
 </script>
 
-<div class="mermaid">
-    {code}
+<div>
+    {#if $rendered}
+    <div class="mermaid">
+        <slot/>
+    </div>
+    {:else}
+    <div>
+        <p></p>
+    </div>
+    {/if}
 </div>
 
 <style>

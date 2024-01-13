a7acf9765fc6de456920b21de28cf9511060f31e
Yannick Lange <yannick.lange.94@gmail.com>
2024-01-13 14:27:16 +0100

    rename Code component to Diagram

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script>
     import '$lib/styles/style.css';
     import Portrait from '$lib/components/Portrait.svelte';
-    import Code from '$lib/components/Code.svelte';
+    import Diagram from '$lib/components/Diagram.svelte';
 </script>
 
 <Portrait/>
 <h2>Yannick Lange</h2>
 <p>hello</p>
-<Code>
+<Diagram>
     flowchart TB 
         A & B --> C & D
-</Code>
+</Diagram>
 
